4e2feda9b13a03bfbcfecca4c9da8f4ea87b3634
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-03-14 11:35:10 +0100

    testing model completed

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,16 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 from store.models import Category, Product
 
-class TestCategoriesModel(TestCase):
+# class TestCategoriesModel(TestCase):
 
+#     def setUp(self):
+#         self.data1 = Category.objects.create(name='django', slug='django')
+
+#     def test_category_model_entry(self):
+#         """
+#         Test Category model data insertion/types/field attributes
+#         """
+#         data = self.data1
+#         self.assertTrue(isinstance(data, Category))  
+
+#     def test_category_model_entry(self):
+#         """
+#         Test Category model default name
+#         """
+#         data  = self.data1
+#         self.assertEqual(str(data), 'django')
+
+class TestProductsModel(TestCase):
     def setUp(self):
-        self.data1 = Category.objects.create(name='django', slug='django')
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
+        self.data1 = Product(category_id=1, title='django beginners', created_by_id=1,
+                            slug='django-beginners', price='20.00', image='django')
 
-    def test_category_model_entry(self):
+    def test_products_models_entry(self):
         """
-        Test Category model data insertion/types/field attributes
+            Test product model data insesrtion/types/field attributes
         """
-
         data = self.data1
-        self.assertTrue(isinstance(data, Category))
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'django beginners')
