6736a393ca1312acd140ecc29d4a55116e72b163
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-03-15 10:32:46 +0100

    adding the category template

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,5 @@ app_name = 'store'
 urlpatterns = [
     path('', views.all_products, name='all_products'),
     path('item/<slug:slug>/', views.product_details, name='product_detail'),
+    path('search/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
