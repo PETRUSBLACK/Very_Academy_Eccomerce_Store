79b049265c6bc96518c738a2424e69c025925fbd
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-03-15 11:41:23 +0100

    done with view testing

--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,10 +1,10 @@
 from unittest import skip
 
 from django.test import TestCase
+
 from django.contrib.auth.models import User
-
 from store.models import Category, Product
-
+from django.urls import reverse 
 from django.test import Client 
 
 # @skip('demonstrationg skipping')
@@ -15,12 +15,20 @@ from django.test import Client
 class TestViewResponses(testCase):
     def setUp(self):
         self.c = Client()
+        User.objects.create(username='admin')
+        Category.objects.create(name='django', slug='django')
+        Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
+                                slug='django-beginners', price='20.00', image='django')
 
     def test_url_allowed_hosts(self):
         """
         Test alloweed Hosts
         """
         response = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_url(self):
+        response = self.c.get(reverse('store:product_detail', args=['django-beginners']))
         self.assertEqual(response.status_code, 200)
 
     # def test_homepage_url(self):
