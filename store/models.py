f7b9abfa0ec77f86587652738ae7235bf0a9af86
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-03-20 13:53:36 +0100

    adding the model Manager

--- store/models.py
+++ store/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
+        
+
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
